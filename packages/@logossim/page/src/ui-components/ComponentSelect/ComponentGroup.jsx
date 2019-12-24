6b2732dd3270ff59dcf368d47e1394d5173df622
Renato Böhler <renato.bohler@gmail.com>
2019-12-24 14:22:54 -0300

    refactor(ComponentGroup): organizes code

--- packages/@logossim/page/src/ui-components/ComponentSelect/ComponentGroup.jsx
+++ packages/@logossim/page/src/ui-components/ComponentSelect/ComponentGroup.jsx
@@ -3,6 +3,67 @@ import styled from 'styled-components';
 
 import Chevron from '../Icons/Chevron';
 
+const Container = styled.div`
+  margin-bottom: 32px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 1.3em;
+`;
+
+const HorizontalSeparator = styled.hr`
+  flex-grow: 1;
+  align-self: center;
+
+  margin: 16px;
+  border-top: 1px solid black;
+`;
+
+const IconButton = styled.button`
+  background: none;
+  border: none;
+  width: 50px;
+
+  transform: ${props => (props.isOpen ? 'rotate(180deg)' : 'none')};
+`;
+
+const ComponentsGrid = styled.div`
+  display: ${props => (props.isOpen ? 'grid' : 'none')};
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 8px;
+`;
+
+const ComponentButton = styled.button`
+  display: flex;
+  align-items: center;
+
+  padding: 8px;
+
+  background: rgba(0, 0, 0, 0.05);
+  border: 1px solid gray;
+  border-radius: 5px;
+
+  :hover {
+    background: rgba(0, 0, 0, 0.1);
+    border-style: dashed;
+  }
+`;
+
+const ComponentTitle = styled.h2`
+  font-weight: normal;
+  font-size: 1.2em;
+`;
+
+const ComponentIcon = styled.div`
+  margin-right: 16px;
+`;
+
 const ComponentGroup = ({
   name,
   components,
@@ -10,79 +71,15 @@ const ComponentGroup = ({
 }) => {
   const [isOpen, setOpen] = useState(true);
 
-  const GroupContainer = styled.div`
-    margin-bottom: 32px;
-  `;
-
-  const GroupHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-  `;
-
-  const GroupTitle = styled.div`
-    font-weight: bold;
-    font-size: 1.3em;
-  `;
-
-  const GroupSeparator = styled.hr`
-    flex-grow: 1;
-    align-self: center;
-
-    margin: 16px;
-    border-top: 1px solid black;
-  `;
-
-  const GroupChevron = styled.button`
-    background: none;
-    border: none;
-    width: 50px;
-
-    transform: ${props => (props.isOpen ? 'rotate(180deg)' : 'none')};
-  `;
-
-  const ComponentsGrid = styled.div`
-    display: ${props => (props.isOpen ? 'grid' : 'none')};
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 8px;
-  `;
-
-  const ComponentButton = styled.button`
-    display: flex;
-    align-items: center;
-
-    padding: 8px;
-
-    background: rgba(0, 0, 0, 0.05);
-    border: 1px solid gray;
-    border-radius: 5px;
-
-    :hover {
-      background: rgba(0, 0, 0, 0.1);
-      border-style: dashed;
-    }
-  `;
-
-  const ComponentTitle = styled.h2`
-    font-weight: normal;
-    font-size: 1.2em;
-  `;
-
-  const ComponentIcon = styled.div`
-    margin-right: 16px;
-  `;
-
   return (
-    <GroupContainer>
-      <GroupHeader onClick={() => setOpen(!isOpen)}>
-        <GroupTitle>{name}</GroupTitle>
-        <GroupSeparator />
-        <GroupChevron
-          isOpen={isOpen}
-          onClick={() => setOpen(!isOpen)}
-        >
+    <Container>
+      <Header onClick={() => setOpen(!isOpen)}>
+        <Title>{name}</Title>
+        <HorizontalSeparator />
+        <IconButton isOpen={isOpen} onClick={() => setOpen(!isOpen)}>
           <Chevron />
-        </GroupChevron>
-      </GroupHeader>
+        </IconButton>
+      </Header>
       <ComponentsGrid isOpen={isOpen}>
         {components.map(component => (
           <ComponentButton
@@ -96,7 +93,7 @@ const ComponentGroup = ({
           </ComponentButton>
         ))}
       </ComponentsGrid>
-    </GroupContainer>
+    </Container>
   );
 };
 
