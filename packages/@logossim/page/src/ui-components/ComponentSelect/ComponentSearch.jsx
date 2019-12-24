a9f74fd389438444a568daccb310713777076647
Renato Böhler <renato.bohler@gmail.com>
2019-12-24 14:35:23 -0300

    feat(ComponentSearch): implements search keyboard shortcuts

--- packages/@logossim/page/src/ui-components/ComponentSelect/ComponentSearch.jsx
+++ packages/@logossim/page/src/ui-components/ComponentSelect/ComponentSearch.jsx
@@ -28,41 +28,63 @@ const ComponentSearch = ({
   const [filteredGroups, setFilteredGroups] = useState(groups);
   const [searchText, setSearchText] = useState('');
 
+  const handleSearchChange = value => {
+    setSearchText(value);
+
+    if (!value) {
+      setFilteredGroups(groups);
+      return;
+    }
+
+    const like = new RegExp(`${value}.*`, 'i');
+
+    const newFilteredGroups = groups
+      .map(group => {
+        const hasAnyMatchingComponent = group.components.some(
+          component => component.name.match(like),
+        );
+
+        if (hasAnyMatchingComponent)
+          return {
+            ...group,
+            components: group.components.filter(component =>
+              component.name.match(like),
+            ),
+          };
+        return null;
+      })
+      .filter(group => group != null);
+
+    setFilteredGroups(newFilteredGroups);
+  };
+
+  const handleKeyDown = key => {
+    if (key === 'Escape') {
+      setSearchText('');
+      setFilteredGroups(groups);
+    }
+
+    if (
+      key === 'Enter' &&
+      searchText &&
+      filteredGroups.length === 1 &&
+      filteredGroups[0].components.length === 1
+    ) {
+      handleComponentSelect(filteredGroups[0].components[0]);
+    }
+  };
+
   return (
     <>
       <Header>
         <SearchBar
+          autoFocus
           placeholder="Search components..."
           value={searchText}
-          onChange={({ target: { value = '' } }) => {
-            setSearchText(value);
-
-            if (!value) {
-              setFilteredGroups(groups);
-              return;
-            }
-
-            const like = new RegExp(`${searchText}.*`, 'i');
-
-            const newFilteredGroups = groups
-              .map(group => {
-                const hasAnyMatchingComponent = group.components.some(
-                  component => component.name.match(like),
-                );
-
-                if (hasAnyMatchingComponent)
-                  return {
-                    ...group,
-                    components: group.components.filter(component =>
-                      component.name.match(like),
-                    ),
-                  };
-                return null;
-              })
-              .filter(group => group != null);
-
-            setFilteredGroups(newFilteredGroups);
-          }}
+          onChange={({ target: { value } }) =>
+            handleSearchChange(value)
+          }
+          onKeyDown={({ key }) => handleKeyDown(key)}
         />
         <IconButton last onClick={handleClose}>
           <Close />
