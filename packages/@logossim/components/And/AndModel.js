a5b6f41428681f7adcd58f11997e55281ffe9e6c
Renato Böhler <renato.bohler@gmail.com>
2020-03-11 19:09:16 -0300

    fix(And): fixes calls to addPort

--- packages/@logossim/components/And/AndModel.js
+++ packages/@logossim/components/And/AndModel.js
@@ -10,9 +10,9 @@ export default class AndModel extends BaseModel {
     );
 
     for (let i = 0; i < INPUT_PORTS_NUMBER; i += 1) {
-      this.addPort(`in${i}`);
+      this.addInPort(`in${i}`);
     }
-    this.addPort('out');
+    this.addOutPort('out');
   }
 
   step() {
