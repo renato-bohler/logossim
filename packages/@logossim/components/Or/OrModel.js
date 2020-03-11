07c181971a6c1a9a3b1c90f95f97e401c3c2a6ba
Renato Böhler <renato.bohler@gmail.com>
2020-03-11 19:09:26 -0300

    fix(Or): fixes calls to addPort

--- packages/@logossim/components/Or/OrModel.js
+++ packages/@logossim/components/Or/OrModel.js
@@ -2,9 +2,9 @@ import { BaseModel } from '@logossim/core';
 
 export default class OrModel extends BaseModel {
   initialize() {
-    this.addPort('in0');
-    this.addPort('in1');
-    this.addPort('out');
+    this.addInPort('in0');
+    this.addInPort('in1');
+    this.addOutPort('out');
   }
 
   onSimulationStart() {
