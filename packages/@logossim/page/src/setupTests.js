65cd17363ff106b5cce55c2665215ddfabc44298
Renato Böhler <renato.bohler@gmail.com>
2020-03-20 22:59:57 -0300

    test: adds worker-loader mock

--- packages/@logossim/page/src/setupTests.js
+++ packages/@logossim/page/src/setupTests.js
@@ -1,4 +1,11 @@
+/* eslint-disable max-classes-per-file */
 import '@testing-library/jest-dom/extend-expect';
+
+// Mocks `worker-loader`
+jest.mock('../../core/Simulation/simulation.worker.js', () => ({
+  __esModule: true,
+  default: class SimulationWorker {},
+}));
 
 jest.mock('@logossim/core', () => ({
   ...jest.requireActual('@logossim/core'),
