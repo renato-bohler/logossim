23c33e3345b5ee8c6328488128640c784dfa2f34
Renato Böhler <renato.bohler@gmail.com>
2020-03-25 20:29:30 -0300

    fix(Clock): fixes clock icon

--- packages/@logossim/components/Clock/ClockWidget.jsx
+++ packages/@logossim/components/Clock/ClockWidget.jsx
@@ -24,38 +24,22 @@ const Wrapper = styled.div`
   }
 `;
 
-const OutputIndicator = ({ out, periodMs, animateTransition }) => (
-  <svg
-    viewBox="0 0 13.229167 13.229167"
-    height="20"
-    width="20"
-    strokeWidth="2"
-    fill="none"
-    stroke={out.getColor()}
-    style={{
-      position: 'absolute',
-      transform: out.getValue() === 0 ? 'rotateX(180deg)' : 'none',
-      transition: animateTransition
-        ? `calc(${periodMs}ms / 2 * 0.5) ease-in-out`
-        : 'none',
-    }}
-  >
-    <g>
-      <path d="m 1.2811445,6.3517135 v 5.8161325 h 5.235864 V 1.0613205 h 5.4150755 c 0.01864,1.757611 0.01578,3.68107 0.01578,5.350926" />
-    </g>
-  </svg>
-);
-
 const PositionedPort = styled(Port)`
   position: absolute;
   right: -5px;
 `;
 
-export const Shape = ({ size = 30 }) => (
+export const Shape = ({
+  size = 30,
+  output,
+  color,
+  periodMs,
+  animateTransition,
+}) => (
   <svg
     className="shape"
-    width={size}
     height={size}
+    width={size}
     viewBox="0 0 7.9374997 7.9375003"
     fill="var(--body-unselected)"
     stroke="var(--border-unselected)"
@@ -69,6 +53,19 @@ export const Shape = ({ size = 30 }) => (
         width="7.4083333"
       />
     </g>
+    <path
+      stroke={color}
+      strokeWidth={0.75}
+      fill="none"
+      style={{
+        transform: output === 0 ? 'rotateX(180deg)' : 'none',
+        transformOrigin: 'center',
+        transition: animateTransition
+          ? `calc(${periodMs}ms / 2 * 0.4) ease-in-out`
+          : 'none',
+      }}
+      d="M 1.8683545,4.4337648 V 6.14192 H 4.0062447 V 1.79558 h 2.0629007 v 1.7578226"
+    />
   </svg>
 );
 
@@ -89,9 +86,9 @@ const ClockWidget = props => {
         port={out}
         engine={engine}
       />
-      <Shape />
-      <OutputIndicator
-        out={out}
+      <Shape
+        output={out.getValue()}
+        color={out.getColor()}
         periodMs={periodMs}
         animateTransition={periodMs >= 500}
       />
