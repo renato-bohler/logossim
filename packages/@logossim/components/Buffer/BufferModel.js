ed2f9a90895c48291088b8e601908ab1ad4c3cf3
Renato Böhler <renato.bohler@gmail.com>
2020-05-28 00:10:52 -0300

    feat(Buffer): adds DATA_BITS configuration

--- packages/@logossim/components/Buffer/BufferModel.js
+++ packages/@logossim/components/Buffer/BufferModel.js
@@ -1,9 +1,11 @@
 import { BaseModel } from '@logossim/core';
 
 export default class BufferModel extends BaseModel {
-  initialize() {
-    this.addInputPort('in');
-    this.addOutputPort('out');
+  initialize(configurations) {
+    const DATA_BITS = parseInt(configurations.DATA_BITS, 10);
+
+    this.addInputPort('in', DATA_BITS);
+    this.addOutputPort('out', DATA_BITS);
   }
 
   step(input) {
