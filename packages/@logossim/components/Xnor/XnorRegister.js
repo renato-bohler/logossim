327fc351a2544cfff5460c49cc363d16f20eb849
Renato Böhler <renato.bohler@gmail.com>
2020-05-28 00:11:33 -0300

    feat(Xnor): adds DATA_BITS configuration

--- packages/@logossim/components/Xnor/XnorRegister.js
+++ packages/@logossim/components/Xnor/XnorRegister.js
@@ -26,6 +26,38 @@ export default new Component({
       },
     },
     {
+      name: 'DATA_BITS',
+      type: 'select',
+      default: '1',
+      label: 'Data bits',
+      options: [
+        {
+          label: '1 bit',
+          value: '1',
+        },
+        {
+          label: '2 bits',
+          value: '2',
+        },
+        {
+          label: '4 bits',
+          value: '4',
+        },
+        {
+          label: '8 bits',
+          value: '8',
+        },
+        {
+          label: '16 bits',
+          value: '16',
+        },
+        {
+          label: '32 bits',
+          value: '32',
+        },
+      ],
+    },
+    {
       name: 'MULTIPLE_INPUT_BEHAVIOR',
       type: 'select',
       default: 'ONE',
