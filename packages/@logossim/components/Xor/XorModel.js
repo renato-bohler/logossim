8644236b80ec11f3716ad06f370621e25599dcd1
Renato Böhler <renato.bohler@gmail.com>
2020-05-30 22:46:29 -0300

    fix(Nor): fixes step logic do consider DATA_BITS

--- packages/@logossim/components/Xor/XorModel.js
+++ packages/@logossim/components/Xor/XorModel.js
@@ -3,30 +3,47 @@ import { BaseModel } from '@logossim/core';
 export default class XorModel extends BaseModel {
   initialize(configurations) {
     this.behavior = configurations.MULTIPLE_INPUT_BEHAVIOR;
+    this.bits = parseInt(configurations.DATA_BITS, 10);
 
     const INPUT_PORTS_NUMBER = parseInt(
       configurations.INPUT_PORTS_NUMBER,
       10,
     );
-    const DATA_BITS = parseInt(configurations.DATA_BITS, 10);
 
     for (let i = 0; i < INPUT_PORTS_NUMBER; i += 1) {
-      this.addInputPort(`in${i}`, DATA_BITS);
+      this.addInputPort(`in${i}`, this.bits);
     }
-    this.addOutputPort('out', DATA_BITS);
+    this.addOutputPort('out', this.bits);
   }
 
-  // TODO: adjust step logic to consider DATA_BITS
+  exclusiveOrAt(values, index) {
+    const mask = 0b1 << index;
+
+    const sum = values
+      .map(value => ((value & mask) > 0 ? 1 : 0))
+      .reduce((acc, curr) => acc + curr);
+
+    return sum === 1 ? 1 : 0;
+  }
+
   step(input) {
-    const numberOfOnInputs = Object.values(input).filter(
-      value => value === 1,
-    ).length;
+    const values = Object.values(input);
 
     switch (this.behavior) {
       case 'ONE':
-        return { out: numberOfOnInputs === 1 ? 1 : 0 };
+        return {
+          out: parseInt(
+            [...new Array(this.bits)]
+              .map((_, index) => this.exclusiveOrAt(values, index))
+              .reverse()
+              .join(''),
+            2,
+          ),
+        };
       case 'ODD':
-        return { out: numberOfOnInputs % 2 === 1 ? 1 : 0 };
+        return {
+          out: values.reduce((acc, curr) => curr ^ acc),
+        };
       default:
         return {};
     }
