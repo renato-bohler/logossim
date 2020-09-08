db689b5005f8f59bf8d317b05190e26ce00a8d67
Renato Böhler <renato.bohler@gmail.com>
2020-09-08 16:18:45 -0300

    fix(Snackbar): fixes snackbar being hidden by Titlebar

--- packages/@logossim/page/src/ui-components/Snackbar/Snackbar.jsx
+++ packages/@logossim/page/src/ui-components/Snackbar/Snackbar.jsx
@@ -37,6 +37,7 @@ const Container = styled.div`
   box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2),
     0px 6px 10px 0px rgba(0, 0, 0, 0.14),
     0px 1px 18px 0px rgba(0, 0, 0, 0.12);
+  z-index: 2;
 `;
 
 const Message = styled.span`
