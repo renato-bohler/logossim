2387b578585f8d1c5cd57f79864f1f600ff09201
Renato Böhler <renato.bohler@gmail.com>
2020-09-13 22:19:18 -0300

    test(Switch): initial component test implementation

---
+++ packages/@logossim/components/Switch/__tests__/SwitchModel.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-new */
+import SwitchModel from '../SwitchModel';
+
+it('should add ports on initialization', () => {
+  const spy = jest.spyOn(SwitchModel.prototype, 'addOutputPort');
+  spy.mockImplementation(() => {});
+
+  new SwitchModel();
+
+  expect(spy).toHaveBeenCalledWith('out');
+});
+
+it('should emit on simulation start', () => {
+  const model = new SwitchModel();
+  const spy = jest.spyOn(model, 'emit');
+  model.onSimulationStart();
+
+  expect(spy).toHaveBeenCalledWith({ out: 0 });
+});
+
+it('should toggle to 1 upon clicking when value is 0', () => {
+  const model = new SwitchModel();
+  const emitSpy = jest.spyOn(model, 'emit');
+  const outputSpy = jest.spyOn(model, 'getOutput');
+  outputSpy.mockImplementation(() => 0);
+
+  model.onClick();
+
+  expect(emitSpy).toHaveBeenCalledWith({ out: 1 });
+});
+
+it('should toggle to 0 upon clicking when value is 1', () => {
+  const model = new SwitchModel();
+  const emitSpy = jest.spyOn(model, 'emit');
+  const outputSpy = jest.spyOn(model, 'getOutput');
+  outputSpy.mockImplementation(() => 1);
+
+  model.onClick();
+
+  expect(emitSpy).toHaveBeenCalledWith({ out: 0 });
+});
