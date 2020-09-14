a8e35a93221443d9ebbf1b5449f47e231013a20b
Renato Böhler <renato.bohler@gmail.com>
2020-09-13 22:15:49 -0300

    test(Clock): initial component test implementation

---
+++ packages/@logossim/components/Clock/__tests__/ClockWidget.test.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import ClockModel from '../ClockModel';
+import ClockWidget from '../ClockWidget';
+
+const { engine } = global;
+
+it('should have 1 output port', () => {
+  const model = new ClockModel('Clock', {
+    FREQUENCY_HZ: 1,
+    HIGH_DURATION: 1,
+    LOW_DURATION: 1,
+  });
+
+  const { container } = render(
+    <ClockWidget model={model} engine={engine} />,
+  );
+
+  const ports = container.querySelectorAll('[data-name=out]');
+  expect(ports).toHaveLength(1);
+});
+
+it('should display off value', () => {
+  const model = new ClockModel('Clock', {
+    FREQUENCY_HZ: 1,
+    HIGH_DURATION: 1,
+    LOW_DURATION: 1,
+  });
+  model.getPort('out').setValue(0);
+
+  const { getByTestId } = render(
+    <ClockWidget model={model} engine={engine} />,
+  );
+  const decoration = getByTestId('decoration');
+
+  expect(decoration).toHaveStyle('transform: rotateX(180deg)');
+});
+
+it('should display on value', () => {
+  const model = new ClockModel('Clock', {
+    FREQUENCY_HZ: 1,
+    HIGH_DURATION: 1,
+    LOW_DURATION: 1,
+  });
+  model.getPort('out').setValue(1);
+
+  const { getByTestId } = render(
+    <ClockWidget model={model} engine={engine} />,
+  );
+  const decoration = getByTestId('decoration');
+
+  expect(decoration).toHaveStyle('transform: none');
+});
