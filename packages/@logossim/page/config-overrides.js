ca2110a727548a5d89227bfa60b0fe2443ad4d1a
Renato Böhler <renato.bohler@gmail.com>
2020-09-13 22:10:50 -0300

    chore: updates config-overrides to allow testing on @logossim/components

--- packages/@logossim/page/config-overrides.js
+++ packages/@logossim/page/config-overrides.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const rewireBabelLoader = require('react-app-rewire-babel-loader');
 
 const fs = require('fs');
@@ -7,17 +8,31 @@ const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath =>
   path.resolve(appDirectory, relativePath);
 
-module.exports = function override(config) {
-  config.module.rules.push({
-    test: /simulation\.worker\.js$/,
-    use: {
-      loader: 'worker-loader',
-      options: { name: 'SimulationWorker.[hash].js' },
-    },
-  });
+module.exports = {
+  webpack: config => {
+    config.module.rules.push({
+      test: /simulation\.worker\.js$/,
+      use: {
+        loader: 'worker-loader',
+        options: { name: 'SimulationWorker.[hash].js' },
+      },
+    });
 
-  return rewireBabelLoader.include(config, [
-    resolveApp('../core'),
-    resolveApp('../components'),
-  ]);
+    return rewireBabelLoader.include(config, [
+      resolveApp('../core'),
+      resolveApp('../components'),
+    ]);
+  },
+  jest: config => {
+    config.rootDir = resolveApp('..');
+    config.testMatch = [
+      '<rootDir>/page/src/**/__tests__/**/*.test.{js,jsx}',
+      '<rootDir>/components/**/__tests__/**/*.test.{js,jsx}',
+    ];
+    config.collectCoverageFrom = ['**/*.{js,jsx}'];
+    config.coverageDirectory = resolveApp('../../../coverage');
+    config.setupFilesAfterEnv = ['<rootDir>/page/src/setupTests.js'];
+    config.roots = ['<rootDir>/page/src', '<rootDir>/components'];
+    return config;
+  },
 };
