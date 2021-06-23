83e8bb8803f720ab54b0f2688c5fc96854104f3c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-23 14:30:59 +0000

    Deploy renato-bohler/logossim to github.com/renato-bohler/logossim.git:gh-pages

---
+++ precache-manifest.138e565ad35c971b6c5c10f9a27f5fc0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae202b9ddac950bbda5f793d4498a0c",
+    "url": "https://renato-bohler.github.io/logossim/SimulationWorker.d18186ab89fe7b76f121.js"
+  },
+  {
+    "revision": "0271991d6da8f2451c6a0126b3d56396",
+    "url": "https://renato-bohler.github.io/logossim/index.html"
+  },
+  {
+    "revision": "2cb028e31c7ab7d8ef81",
+    "url": "https://renato-bohler.github.io/logossim/static/css/2.fbd218b0.chunk.css"
+  },
+  {
+    "revision": "0c67407b696d2182eb7e",
+    "url": "https://renato-bohler.github.io/logossim/static/css/main.b19bc4fa.chunk.css"
+  },
+  {
+    "revision": "2cb028e31c7ab7d8ef81",
+    "url": "https://renato-bohler.github.io/logossim/static/js/2.56b4bb6a.chunk.js"
+  },
+  {
+    "revision": "aa8ce77ce88c7324629ab8a0984467ad",
+    "url": "https://renato-bohler.github.io/logossim/static/js/2.56b4bb6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c67407b696d2182eb7e",
+    "url": "https://renato-bohler.github.io/logossim/static/js/main.1ce65df2.chunk.js"
+  },
+  {
+    "revision": "ef382bfc6f4779b0a9aa",
+    "url": "https://renato-bohler.github.io/logossim/static/js/runtime-main.17ce1b17.js"
+  },
+  {
+    "revision": "e3ec32b9b22d17eeb2eadc5e9b5cbc36",
+    "url": "https://renato-bohler.github.io/logossim/static/media/bifurcation.e3ec32b9.gif"
+  },
+  {
+    "revision": "94343183837e1c43b9be51ed5d5a2da2",
+    "url": "https://renato-bohler.github.io/logossim/static/media/context-menu.94343183.gif"
+  },
+  {
+    "revision": "2d0f910c2cb97460e4cd900c425eed3f",
+    "url": "https://renato-bohler.github.io/logossim/static/media/simulation.2d0f910c.gif"
+  },
+  {
+    "revision": "e7e521dab809f1b350e19eab61bdff4b",
+    "url": "https://renato-bohler.github.io/logossim/static/media/wire.e7e521da.gif"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://renato-bohler.github.io/logossim/precache-manifest.138e565ad35c971b6c5c10f9a27f5fc0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://renato-bohler.github.io/logossim/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
